6cc613b144c0545006ff220c5be61a3820bb78f8
PrincePandeycode <pandeyprince4723@gmail.com>
2023-09-07 16:29:17 +0530

    new features

---
+++ src/module1.mjs
@@ -0,0 +1,8 @@
+
+const a = "Prince Pandey";
+const b = "subham ";
+const c = "Harry";
+
+export default c;
+export {a};
+export {b};
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import hi ,{a,b} from "./module1.mjs"
+console.log(hi)
+console.log(a)
+console.log(b)
