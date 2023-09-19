d2c0411cca98c83d9300825c80d90a3d6f11d439
PrincePandeycode <pandeyprince4723@gmail.com>
2023-09-19 22:04:01 +0530

    i have done few routings but not added about on navbar

--- src/App.js
+++ src/App.js
@@ -1,65 +1,84 @@
 import "./App.css";
-// import About from "./components/About";
 import About2 from "./components/About2";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 
 import Textform from "./components/Textform";
 import React, { useState } from 'react'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
-
-
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [mode,setmode] = useState("light")
-  const [text,textmode] = useState("light")
-  const [navtext,setnav] = useState("dark")
-  const [alert,setAlert] = useState(null)
+  const [mode, setmode] = useState("light");
+  const [text, textmode] = useState("light");
+  const [navtext, setnav] = useState("dark");
+  const [alert, setAlert] = useState(null);
 
-  
-  const showAlert = (message,type) =>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg : message,
-      type :type
-    })
+      msg: message,
+      type: type
+    });
     setTimeout(() => {
-      setAlert(null)
-      
+      setAlert(null);
     }, 1100);
-  }
+  };
 
-  const tooglemode = () =>{
-    if(mode === 'light'){
-      setmode('dark')
-      textmode('light')
-      document.body.style.backgroundColor = "black"
-      setnav("light")
-      showAlert("dark mode is here ","success")
-     
+  const tooglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      textmode("light");
+      document.body.style.backgroundColor = "black";
+      setnav("light");
+      showAlert("dark mode is here ", "success");
+    } else {
+      setmode("light");
+      textmode("dark");
+      setnav("dark");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode is here ", "success");
     }
-    else{
-      setmode('light')
-      textmode('dark')
-      setnav("dark")
-    
-      document.body.style.backgroundColor = "white"
-      showAlert("light mode is here ","success")
-     
-    }
-  }
+  };
+
+
+
   return (
     <>
-      <Navbar title = "Textutils" Home = "Home" mode={mode} tooglemode ={tooglemode} text = {text} setnav = {navtext} / >
-        <Alert alertText = "this site is awesome" alert = {alert}/>
-      {/* <Navbar/>   */}
-      
-      <Textform heading = "Enter text here" alert = {showAlert} mode={mode} nameOfBtn1 ="conert to uppercase" nameOfBtn2 ="conert to Lowecase" nameOfBtn3 = "copy Text" nameOfBtn4 = "Paste here" nameOfBtn5 = "clear"/>
+    
 
-    {/* <About/> */}
-    <About2 mode = {mode}/>
+<Router>
+        <Navbar  title="Textutils"
+          Home="Home"
+          mode={mode}
+          tooglemode={tooglemode}
+          text={text}
+          setnav={navtext}/>
+
+        <Alert alertText="this site is awesome" alert={alert}></Alert>
+        <div className="container my-5">
+          <Routes>
+          <Route path='/About' element={<About2/>} />
+          <Route path='/' element={<Textform  heading="Enter text here"
+                alert={showAlert}
+                mode={mode}
+                nameOfBtn1="conert to uppercase"
+                nameOfBtn2="conert to Lowecase"
+                nameOfBtn3="copy Text"
+                nameOfBtn4="Paste here"
+                nameOfBtn5="clear" />} />
+               
+          </Routes>
+          
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
-
